7f59a618f20af87d2674386555252117dafba9f2
Adam Colton <adamcolton@gmail.com>
2016-12-31 16:37:22 -0500

    Adding package comment

---
+++ doc.go
@@ -0,0 +1,5 @@
+// Package merkle stores resources as Merkle Trees. It should provide security
+// for data at rest, protecting both the data and the meta-data while storing
+// the information in a way that makes it easy to fulfil request for partial
+// resources with validation.
+package merkle
